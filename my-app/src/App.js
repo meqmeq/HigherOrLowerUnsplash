cb092eca490aad66c644357bb5dcaa67067561b9
Marc <mecquilatan@gmail.com>
2021-07-18 18:52:31 -0400

    Added score and added basic hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navvy from "./Components/Navvy";
 import Imagecontainer from "./Components/Imagecontainer";
@@ -6,18 +7,22 @@ import test2 from "./ImageTest/test2.jpg";
 import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
+  const [score, updateScore] = useState(0);
+
   return (
     <>
       <Container fluid>
         <Navvy />
-
         <Row>
           <Col>
-            <Imagecontainer image={test1} />
+            <Imagecontainer image={test1} title="Doggo" />
           </Col>
           <Col>
-            <Imagecontainer image={test2} />
+            <Imagecontainer image={test2} title="Cateo" />
           </Col>
+        </Row>
+        <Row>
+          <h1>Score: {score}</h1>
         </Row>
       </Container>
     </>
--- my-app/src/Components/Imagecontainer.js
+++ my-app/src/Components/Imagecontainer.js
@@ -20,12 +20,12 @@ const Imagecontainer = (props) => {
         fluid
       />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title style={{ textAlign: "center" }}>{props.title}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">This one</Button>
       </Card.Body>
     </Card>
   );
